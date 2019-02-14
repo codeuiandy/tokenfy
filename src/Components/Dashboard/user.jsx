6ac66784a537deb87f2e73f11fa9f0324fe8fa91
Timmy <adefeyitimi@gmail.com>
2019-02-14 12:29:06 +0100

    Created User Component

---
+++ src/Components/Dashboard/user.jsx
@@ -0,0 +1,10 @@
+import React,{useState} from 'react';
+import {Layout} from '../Layout/layout';
+
+export default function user() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
