b23443af82a8f0c3d117fb7aa76ab512da2de4b1
Timmy <adefeyitimi@gmail.com>
2020-11-10 01:11:29 +0100

    Created Reviews jsx component

---
+++ src/Components/Reviews/reviews.jsx
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+import {Layout} from '../Layout/layout'
+import './reviews.css'
+// import CuponsTable from '../Tables/customTable'
+export default function Reviews(props) {
+    return (
+        <div>
+            <Layout NavRouter={props.location.pathname}>
+                
+                           
+
+            </Layout>
+        </div>
+    )
+}
+
+
