68f4a5daf798bbe3b1b730e31b99c3033762e6bd
Timmy <adefeyitimi@gmail.com>
2020-11-13 20:52:32 +0100

    Created user component

--- src/Components/Dashboard/user.jsx
+++ src/Components/Dashboard/user.jsx
@@ -1,10 +1,12 @@
 import React,{useState} from 'react';
 import {Layout} from '../Layout/layout';
 
-export default function user() {
+export default function User(props) {
     return (
         <div>
-            
+            <Layout NavRouter={props.location.pathname}>
+                <h1>User</h1>
+            </Layout>
         </div>
     )
 }
