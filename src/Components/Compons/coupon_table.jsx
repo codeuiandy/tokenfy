73dceb4f93e8cc65e0940713ee05c18fb61c40b7
Timmy Adeyeloja <33554538+FeyiTimi@users.noreply.github.com>
2020-11-16 00:16:41 +0100

    Created table data and rendered it in coupon component

--- src/Components/Compons/coupon_table.jsx
+++ src/Components/Compons/coupon_table.jsx
@@ -2,89 +2,111 @@ import React,{useState} from 'react';
 import {useTable} from 'react-table';
 
 export default function Coupon_table() {
-    const data = React.useMemo(()=>{
-    [
-        {
-        merchant:'Kim Parhish',
-        approvalID:'Adekunle Timilehin',
-        type:'Cashback',
-        limits:'178',
-        category:'Health & Finance',
-        expiring:'20/10/2021',
-        status:'Inactive'
-        },
-        {
-        merchant:'Uber',
-        approvalID:'Adekunle Timilehin',
-        type:'Cashback',
-        limits:'178',
-        category:'Travels',
-        expiring:'20/10/2021',
-        status:'Active'
-        },
-        {
-        merchant:'Tastee',
-        approvalID:'Alo Jeremiah',
-        type:'Online',
-        limits:'178',
-        category:'Dining & Takeout',
-        expiring:'Not Stated',
-        status:'Pending'
-        },
-        {
-        merchant:'Jumia',
-        approvalID:'Alo Jeremiah',
-        type:'Online',
-        limits:'178',
-        category:'Clothing',
-        expiring:'Not Stated',
-        status:'Pending'
-        },
-        {
-        merchant:'Oanda',
-        approvalID:'Odeyemi Peace',
-        type:'In-store',
-        limits:'178',
-        category:'Travels',
-        expiring:'20/10/2021',
-        status:'Active'
-        },
-        {
-        merchant:'Echefu Flourish',
-        approvalID:'Vaseline',
-        type:'In-store',
-        limits:'178',
-        category:'Makeup & Skincare',
-        expiring:'20/10/2021',
-        status:'Active'
-        },
-        {
-        merchant:'Kim Parhish',
-        approvalID:'Adekunle Timilehin',
-        type:'Cashback',
-        limits:'178',
-        category:'Health & Finance',
-        expiring:'20/10/2021',
-        status:'Inactive'
-        }
-    ]
-    });
-    const columns = React.useMemo(()=>{
+    const data = React.useMemo(() =>
         [
             {
-                Header:'Merchant',
-                columns:[
-                    {
-                        Header:'Merchant',
-                        accessor:'merchant'       
-                    }
-                ]
-            }
-        ]
-    });
-    return (
-        <div>
-            
-        </div>
+                merchant:'RX Pharmacy',
+                approvalID:'Adekunle Timilehin',
+                type:'Cashback',
+                Limits:178,
+                Category:'Health & Finance',
+                expiryDate:'20/10/2021',
+                status:'Inactive'
+            },
+        ],
+        []
     )
+    const columns = React.useMemo(
+        () => [
+        {
+            Header: 'Merchant',
+            columns: [
+                {
+                    accessor:'merchant'
+                }
+            ],
+        },
+        {
+            Header: 'Approved By',
+            columns: [
+                {
+                accessor: 'approvalID',
+                }
+            ],
+        },
+        {
+            Header:'Type',
+            columns:[
+                {
+                    accessor:'type'
+                }
+            ]
+        },
+        {
+            Header:'Limits',
+            columns:[
+                {
+                    accessor:'Limits'
+                }
+            ]
+        },
+        {
+            Header:'Category',
+            columns:[
+                {
+                    accessor:"Category"
+                }
+            ]
+        },
+        {
+            Header:'Expiring',
+            columns: [
+                {
+                    accessor:"expiryDate"
+                }
+            ]
+        },
+        {
+            Header:'Status',
+            columns:[
+                {
+                    accessor:'status'
+                }
+            ]
+        }
+        ],
+        []
+       )
+       const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+       } = useTable({ columns, data })
+       return (
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map(row => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+       )
 }
